43766c5c342d3173ec831b52d4bf9c8c815d594f
Paty1223 <josselinnieto23@gmail.com>
2023-04-27 20:46:06 -0600

    Actualizar

---
+++ app.js
@@ -0,0 +1,63 @@
+const textInputField = document.querySelector('#text-input-field');
+const agregarButton = document.querySelector('#agregar-button');
+const todosContainer = document.querySelector('.todos-container');
+
+agregarButton.addEventListener('click', ()=> {
+if (textInputField.value.trim().length == '')
+    return;
+    
+
+
+    
+    const todoItemContainer = document.createElement('div');
+    todoItemContainer.classList.add('todo-item-container');
+
+    todosContainer.appendChild(todoItemContainer);
+
+    const todoText = document.createElement('p');
+    todoText.id = 'todo-text';
+    todoText.innerText = textInputField.value;
+    todoItemContainer.appendChild(todoText);
+
+    todoText.addEventListener('dblclick', ()=>{
+        todoText.classList.add('line-through');
+        editarButton.setAttribute("disabled", "disable");
+    })
+
+    const editarButton = document.createElement('button');
+    editarButton.id = 'editar-button';
+
+    const  editarImage = document.createElement('img');
+    editarImage.src = 'edita.png';
+    editarButton.appendChild(editarImage);
+    todoItemContainer.appendChild(editarButton)
+    
+    editarButton.addEventListener('click', ()=>{
+        textInputField.value = todoText.innerText;
+        const parent = editarButton.parentElement;
+        parent.parentElement.removeChild(parent);
+    });
+
+    const eliminarButton = document.createElement('button');
+    eliminarButton.id = 'eliminar-button';
+
+    const eliminarImage = document.createElement('img');
+    eliminarImage.src = 'borrar.png';
+    eliminarButton.appendChild(eliminarImage);
+    todoItemContainer.appendChild(eliminarButton);
+
+    eliminarButton.addEventListener('click',()=>{
+        const parent = eliminarButton.parentElement;
+        parent.parentElement.removeChild(parent);
+
+    carrito.children[carrito.children.length-1].children[3].innerHTML=""
+    });
+    
+
+
+
+
+
+    textInputField.value = "";
+
+});
